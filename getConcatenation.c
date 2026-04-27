f9fe14052470ce2fdabf819f2aad2efbfd7d8843
youssef-benchaaboun <youssef.benchaaboun1@gmail.com>
2026-04-27 18:07:03 +0000

    leetcode

--- getConcatenation.c
+++ getConcatenation.c
@@ -1,8 +1,20 @@
-int* getConcatenation(int* nums, int numsSize, int* returnSize) {
-    int *ret;
-    ret=malloc(numsSize*sizeof(int)*2);
-    memcpy(ret,nums,numsSize*sizeof(int));
-    memcpy(ret+numsSize*sizeof(int),nums,numsSize*sizeof(int));
-    return ret;
-    
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int	*getConcatenation(int *nums, int numsSize, int *returnSize)
+{
+	int	*ret;
+	int	i;
+
+	*returnSize = 2 * numsSize;
+	ret = malloc(numsSize * sizeof(int) * 2);
+	i = 0;
+	while (i < numsSize)
+	{
+		ret[i] = nums[i];
+		ret[i + numsSize] = nums[i];
+		i++;
+	}
+	return (ret);
 }
